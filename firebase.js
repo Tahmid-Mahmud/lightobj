2bee79b8b648cd93271159f5e40ed867f3fc7d4a
Tahmid Mahmud <73707938+Tahmid-Mahmud@users.noreply.github.com>
2021-11-19 21:05:29 +0600

    B

--- firebase.js
+++ firebase.js
@@ -1,43 +1,52 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+let firebaseConfig = {
     apiKey: "AIzaSyAxW0ZX1klHmQ4JfiYo_L4VVdaf8BOOq_A",
-    authDomain: "progress-6b92a.firebaseapp.com",
+    authDomain: "",
     databaseURL: "https://progress-6b92a-default-rtdb.firebaseio.com",
-    projectId: "progress-6b92a",
-    storageBucket: "progress-6b92a.appspot.com",
+    projectId: "",
+    storageBucket: "",
     messagingSenderId: "234616315547",
     appId: "1:234616315547:web:d0f2aab2f4e1c3938df03e",
     measurementId: "G-2XP435YJ0Y"
 };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-var sub=["Mathematics", "English", "Science"];
-
 function captureAll(token) {
-    for (let i = 0; i < sub.length; i++) {
-        firebase.database().ref(token+" - Subject/"+sub[i]).on('value', function(snapshot){
-            var all = snapshot.val();
-            console.log("title is: "+all.title);
-            console.log("descript is: "+all.description);
-        });
+    localStorage.setItem("isLogin", token);
+    document.getElementById("lBtn").innerHTML="Logout";
+    document.getElementById("lBtn").onclick=function(){
+        localStorage.clear();
+        document.getElementById("lBtn").innerHTML="Login";
+        document.getElementById("lBtn").onclick=function(){
+            $("#exampleModal").modal('show');
+        }
     };
 }
+
 
 function login() {
     var username = document.getElementById("user").value;
     var passValue = document.getElementById("pass").value;
     username="Tahmid-Mahmud";
-    passValue="t@hmid_lightobj"
+    passValue="progress-6b92a";
+
+    // Initialize Firebase
+    firebaseConfig["authDomain"]=passValue+".firebaseapp.com";
+    firebaseConfig["projectId"]=passValue;
+    firebaseConfig["storageBucket"]=passValue+".appspot.com";
+    if (firebase.apps.length === 0) {
+        firebase.initializeApp(firebaseConfig);
+    }
+
+    // Accessiblity
     firebase.database().ref(username).on('value', function(snapshot){
         var password=snapshot.val().password;
         if (passValue==password){
-            document.getElementById("alert").style.display="block";
+            // document.getElementById("alert").style.display="block";
             $("#exampleModal").modal('toggle');
-            captureAll(passValue);
+            captureAll(passValue[passValue.length-2]+passValue[passValue.length-1]);
         }else {
             console.log("Login failed");
+            document.getElementById("error").style.display="inline-block";
         }
     });
 }
